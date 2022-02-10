2d3427b39f8f6b5f2fa0895d5b6cfdf09f346356
ruukichi <ngovietlc93@gmail.com>
2022-02-10 11:19:11 +0700

    add

---
+++ src/com/codegym/OctObserver.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+public class OctObserver extends Observer {
+    public OctObserver(Subject subject) {
+        this.subject = subject;
+        this.subject.add(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println(" " + Integer.toOctalString(subject.getState()));
+    }
+}
